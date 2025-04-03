f5d4e26d787ef87ece7f3162a691718efe3812ab
RUTURAJ1989 <115629673+RUTURAJ1989@users.noreply.github.com>
2025-04-03 04:42:08 +0000

    Added Firebase auth config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,17 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDaHHjMdchgRKCq-xDTzT2p1-WBP8KI9h4",
-    authDomain: "ipl-auction-game-89635.firebaseapp.com",
-    databaseURL: "https://ipl-auction-game-89635-default-rtdb.firebaseio.com",
-    projectId: "ipl-auction-game-89635",
-    storageBucket: "ipl-auction-game-89635.firebasestorage.app",
-    messagingSenderId: "1068660815248",
-    appId: "1:1068660815248:web:29f41524ab94c7de952f15"
+  apiKey: "AIzaSyDaHHjMdchgRKCq-xDTzT2p1-WBP8KI9h4",
+  authDomain: "ipl-auction-game-89635.firebaseapp.com",
+  databaseURL: "https://ipl-auction-game-89635-default-rtdb.firebaseio.com",
+  projectId: "ipl-auction-game-89635",
+  storageBucket: "ipl-auction-game-89635.firebasestorage.app",
+  messagingSenderId: "1068660815248",
+  appId: "1:1068660815248:web:29f41524ab94c7de952f15",
+  measurementId: "G-BNJZMTCGVT"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
